b4fdf47400f62375eab81de89fdf6212d9bbd0ca
lihucheng1998 <211674920@qq.com>
2022-06-16 13:41:39 +0800

    测试

---
+++ src/main/java/com/example/study/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.example.study.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 21167
+ */
+@RestController
+public class TestController {
+}
